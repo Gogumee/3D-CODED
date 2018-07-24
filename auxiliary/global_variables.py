737f1b538559e8792ca714ac7a9762bb60868b2d
groxordi <thibaultgroueix@orange.fr>
2018-07-24 23:45:41 +0200

    gogo

--- auxiliary/global_variables.py
+++ auxiliary/global_variables.py
@@ -5,8 +5,8 @@ global opt
 global mesh_ref
 global mesh_ref_LR
 # load template at high and low resolution
-mesh_ref = trimesh.load("/home/thibault/Downloads/MPI-FAUST/training/ref/reg_color_ref_HR.ply", process=False)
-mesh_ref_LR = trimesh.load("/home/thibault/Downloads/MPI-FAUST/training/ref/reg_color_ref.ply", process=False)
+mesh_ref = trimesh.load("./data/reg_color_ref_HR.ply", process=False)
+mesh_ref_LR = trimesh.load("./data/reg_color_ref.ply", process=False)
 red_LR = np.load("./data/red_LR.npy").astype("uint8")
 green_LR = np.load("./data/green_LR.npy").astype("uint8")
 blue_LR = np.load("./data/blue_LR.npy").astype("uint8")
