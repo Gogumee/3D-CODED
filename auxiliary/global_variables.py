ea65faade8e0a4430c80208de020eabc330fe710
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2018-07-24 22:57:21 +0200

    dirty commit

--- auxiliary/global_variables.py
+++ auxiliary/global_variables.py
@@ -5,5 +5,5 @@ global opt
 global mesh_ref
 global mesh_ref_LR
 # load template at high and low resolution
-mesh_ref = pymesh.load_mesh("/home/thibault/Downloads/MPI-FAUST/training/ref/reg_color_ref_HR.ply")
-mesh_ref_LR = pymesh.load_mesh("/home/thibault/Downloads/MPI-FAUST/training/ref/reg_color_ref.ply")
+mesh_ref = trimesh.load("/home/thibault/Downloads/MPI-FAUST/training/ref/reg_color_ref_HR.ply", process=False)
+mesh_ref_LR = trimesh.load("/home/thibault/Downloads/MPI-FAUST/training/ref/reg_color_ref.ply", process=False)
