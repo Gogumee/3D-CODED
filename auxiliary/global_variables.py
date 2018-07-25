bc485a615487cc9c7b9fe8121b1d3f78d0e289b6
ThibaultGROUEIX <thibaultgroueix@orange.fr>
2018-07-25 13:02:58 +0200

    better orga

--- auxiliary/global_variables.py
+++ auxiliary/global_variables.py
@@ -5,11 +5,13 @@ global opt
 global mesh_ref
 global mesh_ref_LR
 # load template at high and low resolution
-mesh_ref = trimesh.load("./data/reg_color_ref_HR.ply", process=False)
-mesh_ref_LR = trimesh.load("./data/reg_color_ref.ply", process=False)
-red_LR = np.load("./data/red_LR.npy").astype("uint8")
-green_LR = np.load("./data/green_LR.npy").astype("uint8")
-blue_LR = np.load("./data/blue_LR.npy").astype("uint8")
-red_HR = np.load("./data/red_HR.npy").astype("uint8")
-green_HR = np.load("./data/green_HR.npy").astype("uint8")
-blue_HR = np.load("./data/blue_HR.npy").astype("uint8")
+mesh_ref = trimesh.load("./data/template/template_dense.ply", process=False)
+mesh_ref_LR = trimesh.load("./data/template/template.ply", process=False)
+
+#load colors
+red_LR = np.load("./data/template/red_LR.npy").astype("uint8")
+green_LR = np.load("./data/template/green_LR.npy").astype("uint8")
+blue_LR = np.load("./data/template/blue_LR.npy").astype("uint8")
+red_HR = np.load("./data/template/red_HR.npy").astype("uint8")
+green_HR = np.load("./data/template/green_HR.npy").astype("uint8")
+blue_HR = np.load("./data/template/blue_HR.npy").astype("uint8")
